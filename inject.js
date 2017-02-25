d9cd3e1c5b2afed1df3d6a33ee2f173f9ab82a43
Ricky Han <rickylqhan@gmail.com>
2017-02-25 02:18:49 -0500

    init

---
+++ inject.js
@@ -0,0 +1,8 @@
+
+
+
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "https://txtpen.com/embed.js";
+  script.async = false;
+  document.head.appendChild(script);
